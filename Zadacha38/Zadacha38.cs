fa8080793e8649d6fcebb7af287f017f2f130620
AlexIv <ivlieva.medphyslab.com>
2023-02-18 15:21:35 +0300

    Max and Min added, Max-Min added and works

--- Zadacha38/Zadacha38.cs
+++ Zadacha38/Zadacha38.cs
@@ -4,8 +4,10 @@
 System.Console.WriteLine("Enter massive lenght: "); // Хочу, чтобы размер массива задал пользователь.
 int size = Int32.Parse(System.Console.ReadLine());
 double []OursArray= MassiveOfRandomNumbers(size);
-MassiveAsString (OursArray); /* Вывод массива в консоль не удален 
+/*MassiveAsString (OursArray); //Вывод массива в консоль не удален 
 для удобства проверки дз, хотя по тз выводить сам массив не нужно*/
+double DifferenceBetweenMaximalAndMinimalNumbers = FindMaximalNumberInArray (OursArray) - FindMinimalNumberInArray (OursArray);
+System.Console.WriteLine(DifferenceBetweenMaximalAndMinimalNumbers);
 
 double[] MassiveOfRandomNumbers(int size)
 {
@@ -18,8 +20,34 @@ double[] MassiveOfRandomNumbers(int size)
  return ArrayOfNumbers;
 }
 
-void MassiveAsString (double [] OursArray)
+/*void MassiveAsString (double [] OursArray)
 {
     string ArrayAsString = string.Join(" , ", OursArray); 
     System.Console.WriteLine(ArrayAsString);
+}*/
+
+double FindMinimalNumberInArray (double []OursArray)
+{
+    double MinimalNumber =OursArray[0];
+    for (int index=1; index<OursArray.Length; index++)
+    {
+        if (OursArray[index]<MinimalNumber)
+        {
+            MinimalNumber=OursArray[index];
+        }
+    }
+    return MinimalNumber;
+}
+
+double FindMaximalNumberInArray (double []OursArray)
+{
+    double MaximalNumber =OursArray[0];
+    for (int index=1; index<OursArray.Length; index++)
+    {
+        if (OursArray[index]>MaximalNumber)
+        {
+            MaximalNumber=OursArray[index];
+        }
+    }
+    return MaximalNumber;
 }
