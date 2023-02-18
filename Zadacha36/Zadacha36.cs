25bc32b6650f393ffe533b0fe9421f53e4dc6bea
AlexIv <ivlieva.medphyslab.com>
2023-02-18 14:36:31 +0300

    Code to count sum of numbers in uneven positions works

--- Zadacha36/Zadacha36.cs
+++ Zadacha36/Zadacha36.cs
@@ -5,8 +5,9 @@
 System.Console.WriteLine("Enter massive lenght: "); // Хочу, чтобы размер массива задал пользователь.
 int size = Int32.Parse(System.Console.ReadLine());
 int []OursArray= MassiveOfRandomNumbers(size);
-MassiveAsString (OursArray); 
-System.Console.WriteLine(ToCountEvenNumbersInMassive(OursArray));
+/*MassiveAsString (OursArray); // Вывод массива в консоль не удален 
+для удобства проверки дз, хотя по тз выводить сам массив не нужно*/
+System.Console.WriteLine(ToCountSumOfNumbersInUnevenPositions(OursArray));
 
 int[] MassiveOfRandomNumbers(int size)
 {
@@ -14,13 +15,24 @@ int[] MassiveOfRandomNumbers(int size)
  Random rand=new Random();
  for (int index=0; index<size; index++)
  {
-    ArrayOfNumbers[index]=rand.Next();
+    ArrayOfNumbers[index]=rand.Next(-99,100); // Пусть в массиве лежат только одно- и двузначные числа.
  }
  return ArrayOfNumbers;
 }
 
-void MassiveAsString (int [] OursArray)
+/*void MassiveAsString (int [] OursArray)
 {
     string ArrayAsString = string.Join(" , ", OursArray); 
     System.Console.WriteLine(ArrayAsString);
+}*/
+
+int ToCountSumOfNumbersInUnevenPositions (int [] OursArray)
+{
+ int SumOfNumbersInUnevenPositions=0;
+    for (int index=1; index<OursArray.Length; index=index+2) // Судя по примеру в тз, первой нечетной позицией считать надо ячейку под индексом 1.
+    {
+     SumOfNumbersInUnevenPositions=SumOfNumbersInUnevenPositions+OursArray[index];
+     System.Console.WriteLine(OursArray[index]);
+    }
+    return SumOfNumbersInUnevenPositions;
 }
